9f128282d8d6b9aa801bc46179b1d7a755ab5b4f
[vedhashni] <[vedhavel.1234@gmail.com]>
2021-07-16 12:43:10 +0530

    [Vedhashni] Updated the file with permutation

---
+++ AlgorithmProblems/Permutation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmProblems
+{
+    class Permutation
+    {
+        public static void permutation(String str, int l, int r)
+        {
+            if (l == r)
+                Console.WriteLine(str);
+            else
+            {
+                for (int i = l; i <= r; i++)
+                {
+                    str = swapping(str, l, i);
+                    permutation(str, l + 1, r);
+                    str = swapping(str, l, i);
+                }
+            }
+        }
+
+        public static String swapping(String a,
+                              int i, int j)
+        {
+            char temp;
+            char[] charArray = a.ToCharArray();
+            temp = charArray[i];
+            charArray[i] = charArray[j];
+            charArray[j] = temp;
+            string s = new string(charArray);
+            return s;
+        }
+    }
+}
--- AlgorithmProblems/Program.cs
+++ AlgorithmProblems/Program.cs
@@ -11,6 +11,21 @@ namespace AlgorithmProblems
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Algorithm Programs");
+
+            int option = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("1.Permutation");
+            switch(option)
+            {
+                case 1:
+                    String str = Console.ReadLine();
+                    int len = str.Length;
+                    Permutation.permutation(str, 0, len - 1);
+                    break;
+
+                default:
+                    break;
+
+            }
         }
     }
     }
